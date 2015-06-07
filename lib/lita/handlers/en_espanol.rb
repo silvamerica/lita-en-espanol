143186125fab5106f0e980fd9464aba72b750e7f
Nicholas Silva <nick@silvamerica.com>
2015-06-07 16:35:48 -0700

    Add Support for Changing Language

--- lib/lita/handlers/en_espanol.rb
+++ lib/lita/handlers/en_espanol.rb
@@ -2,17 +2,95 @@ require 'microsoft_translator'
 module Lita
   module Handlers
     class EnEspanol < Handler
+      LANGUAGES = {
+        "ar" => "Arabic",
+        "bs-Latn" => "Bosnian (Latin)",
+        "bg" => "Bulgarian",
+        "ca" => "Catalan",
+        "zh-CHS" => "Chinese Simplified",
+        "zh-CHT" => "Chinese Traditional",
+        "hr" => "Croatian",
+        "cs" => "Czech",
+        "da" => "Danish",
+        "nl" => "Dutch",
+        "en" => "English",
+        "et" => "Estonian",
+        "fi" => "Finnish",
+        "fr" => "French",
+        "de" => "German",
+        "el" => "Greek",
+        "ht" => "Haitian Creole",
+        "he" => "Hebrew",
+        "hi" => "Hindi",
+        "mww" => "Hmong Daw",
+        "hu" => "Hungarian",
+        "id" => "Indonesian",
+        "it" => "Italian",
+        "ja" => "Japanese",
+        "tlh" => "Klingon",
+        "ko" => "Korean",
+        "lv" => "Latvian",
+        "lt" => "Lithuanian",
+        "ms" => "Malay",
+        "mt" => "Maltese",
+        "no" => "Norwegian",
+        "fa" => "Persian",
+        "pl" => "Polish",
+        "pt" => "Portuguese",
+        "otq" => "Querétaro Otomi",
+        "ro" => "Romanian",
+        "ru" => "Russian",
+        "sr-Cyrl" => "Serbian (Cyrillic)",
+        "sr-Latn" => "Serbian (Latin)",
+        "sk" => "Slovak",
+        "sl" => "Slovenian",
+        "es" => "Spanish",
+        "sv" => "Swedish",
+        "th" => "Thai",
+        "tr" => "Turkish",
+        "uk" => "Ukrainian",
+        "ur" => "Urdu",
+        "vi" => "Vietnamese",
+        "cy" => "Welsh",
+        "yua" => "Yucatec Maya"
+      }
+
       config :target_room
       config :source_room
       config :ms_client_id
       config :ms_client_secret
-      config :target_language
+      config :language
 
-      route(/.*/, :translate)
+      route(/.*/, :translate_message)
+      route(/set language to/i, :set_language, command: true)
+      route(/list languages/i, :list_languages, command: true)
 
-      def translate(response)
+      def set_language(response)
+        return unless response.message.source.room == config.target_room
+        input_key = response.args[2..-1].join(" ")
+        # See if someone asked for a language code
+        language = LANGUAGES.select do |key, value|
+          key.downcase == input_key.downcase ||
+          value.downcase == input_key.downcase
+        end
+        # Respond with an error message
+        if language.empty?
+          response.reply(translate("I can not find that language."))
+        else
+          redis.set(:language, language.keys[0])
+          response.reply(translate("I have set the language to #{language.values[0]}"))
+        end
+      end
+
+      def list_languages(response)
+        return unless response.message.source.room == config.target_room
+        response.reply("```" + LANGUAGES.map{|k, v| "#{k} = #{v}"}.join("\n") + "```")
+      end
+
+      def translate_message(response)
+        return if response.user.id.empty?
         return unless response.message.source.room == config.source_room
-        translator = MicrosoftTranslator::Client.new(config.ms_client_id, config.ms_client_secret)
+
         # Assign the message to a temporary variable
         message = response.message.body
         # Find all emoji
@@ -20,13 +98,22 @@ module Lita
         # Replace emoji with untranslatable placeholders
         emoji.each_with_index{|match, index| message.sub!(match, "[[#{index}]]") }
         # Translate the message
-        translated = translator.translate(message, "en", config.target_language, "text/plain")
+        translated = translate(message) || message
         # Put Emoji Back
         emoji.each_with_index{|match, index| translated.sub!("[[#{index}]]", match) }
-        post_message_to_alternate_room(translated, response.message.user)
+        post_message_to_target_room(translated, response.user)
       end
 
-      def post_message_to_alternate_room(message, user)
+      def translate(message)
+        translator = MicrosoftTranslator::Client.new(config.ms_client_id, config.ms_client_secret)
+        translator.translate(message, nil, language, "text/plain")
+      end
+
+      def language
+        redis.get(:language) || config.language
+      end
+
+      def post_message_to_target_room(message, user)
         @adapter ||= robot.send :adapter
         @api ||= Lita::Adapters::Slack::API.new(@adapter.config)
         outgoing_params = {
@@ -50,7 +137,6 @@ module Lita
           return url
         end
       end
-
     end
 
     Lita.register_handler(EnEspanol)
